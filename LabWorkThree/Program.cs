cf3536b472e85d945b87aa6be0f1a442124e04c1
ssplant <76212491+ssplant@users.noreply.github.com>
2021-01-01 22:41:20 +0900

    Add project files.

---
+++ LabWorkThree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LabWorkThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstVariable, secondVariable, tempVariable;
+            Console.WriteLine("Введите первое дробное число:");
+            for (; ; )
+            {
+                if (double.TryParse(Console.ReadLine(), out firstVariable))
+                {
+                    Console.WriteLine("Введите второе дробное число:");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Введите первое дробное число:");
+                    continue;
+                }
+            }
+            for (; ; )
+            {
+                if (double.TryParse(Console.ReadLine(), out secondVariable))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Введите второе дробное число:");
+                    continue;
+                }
+            }
+            Console.WriteLine("Первое число это " + firstVariable + "\r\nВторое число это " + secondVariable);
+            Console.WriteLine("Вы хотите поменять значения чисел местами? (y - Да / n - Нет, выход из программы)");
+            for (; ; )
+            {
+                if (Console.ReadLine() == "y")
+                {
+                    tempVariable = firstVariable;
+                    firstVariable = secondVariable;
+                    secondVariable = tempVariable;
+                    Console.WriteLine("Первое число это " + firstVariable + "\r\nВторое число это " + secondVariable);
+                    Console.WriteLine("Вы хотите поменять значения чисел местами?  (y - Да, n - Нет, выход из программы)");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
